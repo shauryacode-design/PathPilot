28608dd3691717938483355147ae2d5ab77c2156
shauryacode-design <shauryaofficial06@gmail.com>
2026-05-15 22:48:29 +0530

    Initial commit - main changes

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,21 +1,37 @@
 "use client"
 import Image from "next/image";
-// import "../styles/hero.css";
-import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import "../../styles/onboarding.css";
+// import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 
 
 
 export default function Home() {
   return (
-    <div className="dashboard-container">
-      <header className="dashboard-header">
-        <h1>Onboarding</h1>
-        {/* The user button will display here after successful login */}
-        <UserButton />
-      </header>
-      <main>
-        <p>Welcome to your application Onboarding!</p>
-      </main>
+    <div className="onboarding-section">
+      <div className="onboarding-container">
+        <div className="steps-timeline">
+          <div className="steps">
+            <div className="step">
+              <h1>1</h1>
+            </div>
+            <h3>First</h3>
+          </div>
+          <div className="timeline-lines"></div>
+          <div className="steps">
+            <div className="step">
+              <h1>2</h1>
+            </div>
+            <h3>Second</h3>
+          </div>
+          <div className="timeline-lines"></div>
+          <div className="steps">
+            <div className="step">
+              <h1>3</h1>
+            </div>
+            <h3>Third</h3>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
